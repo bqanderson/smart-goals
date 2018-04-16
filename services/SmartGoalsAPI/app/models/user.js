e486ce09c56de77f50c43e25402c8ac9169e547e
bqanderson <bqanderson@me.com>
2018-04-15 19:26:26 -0500

    Complete user model.

--- services/SmartGoalsAPI/app/models/user.js
+++ services/SmartGoalsAPI/app/models/user.js
@@ -13,5 +13,35 @@ const Schema = mogoose.Schema({
     required: true
   },
 
-  clients: [{}]
+  goals: [{}]
 });
+
+// We won't use arrow function here because of automatic lexical scope binding
+
+Schema.pre('save', function (next) {
+  const user = this;
+
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(10, (error, salt) => {
+      if (error) return next(error);
+
+    bcrypt.hash(user.password, salt, (error, hash) => {
+      if (error) return next(error);
+
+      user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+Schema.methods.comparePassword = function (password, callback) {
+  bcrypt.compare(password, this.password, (error, matches) => {
+    if (error) return callback(error);
+    callback(null, matches);
+  });
+};
+
+mogoose.model('User', Schema);
